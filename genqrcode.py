49697cef06aa54ef9edf32fbc0f9a280a7f22b94
M.Sani Ibrahim <mamansani.ibrahim01@gmail.com>
2023-04-29 00:21:48 +0100

    first

--- genqrcode.py
+++ genqrcode.py
@@ -1,15 +1,16 @@
 import qrcode
 
 
-first_name = str(input("Enter your first_name: "))   # Ask the user's first name
-last_name = str(input("Enter your last_name: "))    # Ask for user's last name
+first_name = str(input("Enter your First Name: "))   # Ask the user's first name
+last_name = str(input("Enter your Last Name: "))    # Ask for user's last name
 user_id = input("Enter your ID: ")                  # Ask dor user's ID
 status = str(input("Enter your status: "))          # Ask for user's Status : Jobless or professional
 
-
+# QRcode
 codqr = qrcode.QRCode(version=1, box_size=10, border=4)
-codqr.add_data(f"first_name:{first_name}, last_name: {last_name}, ID: {user_id}, status: {status}")
+codqr.add_data(f"First Name: {first_name}, Last Name: {last_name}, ID: {user_id}, status: {status}")
 codqr.make(fit=True)
 
+# qr image generator
 img = codqr.make_image(fill_color='black', back_color='white')
-img.save(f"{first_name} {last_name}")
+img.save(f"{first_name} {last_name}.png")
