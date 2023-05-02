098a6518447ec3ab802fff1a222662019f9ae9ef
M.Sani Ibrahim <mamansani.ibrahim01@gmail.com>
2023-05-02 11:29:55 +0100

    new changes by using tabnine an AI tool for rapid development.

--- genqrcode.py
+++ genqrcode.py
@@ -1,12 +1,13 @@
-import qrcode
+import qrcode   #qrcode module
 
 
+#Ask user to enter name, last name, id and email
 first_name = str(input("Enter your First Name: "))   # Ask the user's first name
 last_name = str(input("Enter your Last Name: "))    # Ask for user's last name
 user_id = input("Enter your ID: ")                  # Ask dor user's ID
-status = str(input("Enter your status: "))          # Ask for user's Status : Jobless or professional
+email = input("Enter your email: ")        # Ask for user's email
 
-# QRcode
+# QRcode generation function
 codqr = qrcode.QRCode(version=1, box_size=10, border=4)
 codqr.add_data(f"First Name: {first_name}, Last Name: {last_name}, ID: {user_id}, status: {status}")
 codqr.make(fit=True)
